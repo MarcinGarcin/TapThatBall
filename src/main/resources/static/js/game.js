91ece3f037c08d7855e51c5b087e4a7a496debaf
MarcinGarcin <knucias123@gmail.com>
2025-03-31 03:02:13 +0200

    Created simple layout, almost working game, still need to add losing, and increasing level of difficulty

--- src/main/resources/static/js/game.js
+++ src/main/resources/static/js/game.js
@@ -3,23 +3,33 @@ const ctx = canvas.getContext("2d");
 
 canvas.width = parseInt(getComputedStyle(canvas).width);
 canvas.height = parseInt(getComputedStyle(canvas).height);
+ctx.font = "Bold 150px Courier New";
+ctx.fillStyle = "#e4d5b7";
+
+// Ball properties
+const ballSize = canvas.height / 8;
+let ballX = canvas.width / 2 - ballSize / 2;
+let ballY = canvas.height / 2 - ballSize / 2;
+let ballVy = 0;
+let ballVx = 0;
+
+// Game settings
+const gravity = 0.2;
+const bounceFactor = 0.98;
+const fractionFactor = 0.98;
+const horizontalHitFactor = bounceFactor * gravity * 10;
+const verticalHitFactor = bounceFactor * gravity * 100;
+let isPlaying = false;
+let score = 0;
+let scoreX = canvas.width / 2;
+let scoreY = canvas.height / 3;
 
 const img = new Image();
 img.src = "../assets/ball.png";
 
-let ballSize = canvas.height / 8;
-let ballRotation = 0;
-let ballX = canvas.width / 2 - ballSize / 2;
-let ballY = canvas.height / 2 - ballSize / 2;
-let gravity = 0.2;
-let ballVy = 0;
-let ballVx = 0;
-let bounceFactor = 0.9;
-let fractionFactor = 0.98;
-let hitFactor = bounceFactor * gravity * 100;
-let isPlaying = false;
-
 img.onload = function () {
+    calculateTextPosition()
+    ctx.fillText(score.toString(), scoreX, scoreY);
     ctx.drawImage(img, ballX, ballY, ballSize, ballSize);
 };
 
@@ -32,24 +42,30 @@ canvas.addEventListener("mousedown", function (event) {
     checkIfBallWasClicked(event);
 });
 
+
 function animate() {
     if (!isPlaying) return;
+
     calculatePosition();
+    calculateTextPosition()
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+
+    ctx.fillText(score.toString(), scoreX, scoreY);
     ctx.drawImage(img, ballX, ballY, ballSize, ballSize);
+
     requestAnimationFrame(animate);
 }
+
 
 function calculatePosition() {
     ballVy += gravity;
     ballVy *= fractionFactor;
     ballY += ballVy;
     ballX += ballVx;
-    ballRotation = ballVy / 10000;
 
     if (ballY >= canvas.height - ballSize) {
-        ballY = canvas.height - ballSize;
-        ballVy *= -bounceFactor;
+        console.log("you lose")
     }
     if (ballY <= 0) {
         ballY = 0;
@@ -69,11 +85,23 @@ function checkIfBallWasClicked(event) {
     const rect = canvas.getBoundingClientRect();
     const x = event.clientX - rect.left;
     const y = event.clientY - rect.top;
+
     if (x > ballX && x < ballX + ballSize && y > ballY && y < ballY + ballSize) {
-        if (ballVy < 0) {
-            ballVy *= -1;
+        score++;
+        if (x >= ballX + ballSize * (1/3)) {
+            ballVy -= verticalHitFactor;
+            ballVx -= horizontalHitFactor;
+        } else if (x <= ballX + ballSize * (2/3)) {
+            ballVy -= verticalHitFactor;
+            ballVx += horizontalHitFactor;
         } else {
-            ballVy -= hitFactor;
+            ballVy -= verticalHitFactor;
         }
     }
+}
+
+function calculateTextPosition(){
+    scoreTextWidth = ctx.measureText(score.toString()).width;
+    scoreX = canvas.width / 2 - scoreTextWidth / 2;
+    scoreY = canvas.height / 3;
 }
